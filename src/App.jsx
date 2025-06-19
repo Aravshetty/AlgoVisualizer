be07c88181c3fd792d9877b154f3ad24eac60d98
Arav <aravshetty617@gmail.com>
2025-06-19 22:59:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import PathVisualizer from './PathVisualizer/PathVisualizer.jsx';
+
+function App() {
+  return (
+    <div>
+      <PathVisualizer />
+    </div>
+  );
+}
+
+export default App;
+
